b12fa5e82f8623333b18aad4f5e88e58ca26b197
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-07-18 18:01:55 -0500

    Initial site files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shrimp Burger', false);
+
